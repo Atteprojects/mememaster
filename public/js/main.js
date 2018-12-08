065d36191b31829013b2d7432121535abcd7828f
ateyib <atteprojects@gmail.com>
2018-12-07 17:29:59 -0800

    upload image works

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,18 @@ myBtn.addEventListener('click', () => {
 
 
 
+function uplaodImage(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        var uploadedImg = document.getElementById("uploadedImg");
+
+        reader.onload = function (e) {
+            uploadedImg.setAttribute('src', e.target.result);
+            // $('#inputfile').css('display', 'none');
+            // $('#inputFileLabel').css('display', 'none');
+            // $('#imageUplaodBtn').css('display', 'block');
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
